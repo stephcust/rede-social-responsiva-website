5c8dba324c4cff9f6d0ee425b5afc55898005769
Stepheson Custódio <stephesoncustodio@gmail.com>
2022-04-16 15:46:59 -0400

    feat: personalização do tamanho da fonte e da cor do tema

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const mudaItemAtivo = () => {
         item.classList.remove('active');
     })
 }
+//Tema
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+let root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+// ===== Barra Lateral ======= 
 
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
@@ -51,5 +59,91 @@ const searchMessage = () => {
         }
     })
 }
-
 messageSearch.addEventListener('keyup', searchMessage);
+
+//Customização de tema
+//abre
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+//fecha
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModal);
+
+//======= Fontes ========
+
+//remove classe ativa dos seletores de tamanho de fonte
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+    
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '5.4rem');
+        }else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        }else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        }else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        }else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-12rem');
+            root.style.setProperty('--sticky-top-right', '-35rem');
+        }
+        
+        // muda o tamanho da fonte raiz dos elementos HTML
+        document.querySelector('html').style.fontSize = fontSize;
+    })
+    
+})
+//remove classe ativa dos seletores de cor
+const removeActiveColorClass = () => {
+    colorPalette.forEach(color => {
+        color.classList.remove('active');
+    })
+}
+
+//muda cores primárias
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        removeActiveColorClass();
+        let primaryHue;
+
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
+})
