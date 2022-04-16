d59892168fadfbed256363c1e8978d7f9da7d31e
Stepheson Custódio <stephesoncustodio@gmail.com>
2022-04-16 13:13:15 -0400

    feat: pesquisa de chat por mensagem ou por nome

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Barra Lateral
 const menuItems = document.querySelectorAll('.menu-item');
-
+// Mensagens
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+// Busca de mensagens
+const message = document.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
 //remover classe ativa de todos os itens do menu
 const mudaItemAtivo = () => {
@@ -25,6 +28,8 @@ menuItems.forEach(item => {
 })
 
 //Mensagens
+
+//Destacar card de mensagens quando clicado
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
@@ -32,3 +37,19 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000)
 });
+
+//pesquisa de mensagens
+const searchMessage = () => {
+    const val =  messageSearch.value.toLowerCase();
+    message.forEach( chat => {
+        let nome = chat.querySelector('h5').textContent.toLowerCase();
+        let msg = chat.querySelector('p').textContent.toLowerCase();
+        if(nome.includes(val) || msg.includes(val)){
+            chat.style.display = 'flex';
+        }else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+messageSearch.addEventListener('keyup', searchMessage);
